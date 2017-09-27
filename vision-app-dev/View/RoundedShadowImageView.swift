44b4eb9cea1bee8f5fe65f9b0720ee341458d5f9
Nikka-Sher <npiterman@gmail.com>
2017-09-26 23:51:12 -0700

    changes text to classification based on image, ML incorporated

---
+++ vision-app-dev/View/RoundedShadowImageView.swift
@@ -0,0 +1,22 @@
+//
+//  RoundedShadowImageView.swift
+//  vision-app-dev
+//
+//  Created by Konstantine Piterman on 9/25/17.
+//  Copyright © 2017 Konstantine Piterman. All rights reserved.
+//
+
+import UIKit
+
+class RoundedShadowImageView: UIImageView {
+
+    override func awakeFromNib() {
+        self.layer.shadowColor = UIColor.darkGray.cgColor
+        self.layer.shadowRadius = 15
+        self.layer.shadowOpacity = 0.75
+        self.layer.cornerRadius = self.frame.height / 2
+        
+        
+    }
+
+}
